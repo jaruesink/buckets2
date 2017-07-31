5eb18e86f352aa83b9dcdbf71ef9089686a52eeb
Jake Ruesink <jaruesink@gmail.com>
2017-07-30 23:57:49 -0500

    changing some urls to https so mixed content isnt blocked

--- server/models/user.js
+++ server/models/user.js
@@ -17,7 +17,7 @@ const UserSchema = new Schema({
   },
   picture: {
     type: String,
-    default: 'http://b.static.ak.fbcdn.net/rsrc.php/v1/yo/r/UlIqmHJn-SK.gif'
+    default: 'https://b.static.ak.fbcdn.net/rsrc.php/v1/yo/r/UlIqmHJn-SK.gif'
   },
   status: {
     type: String
