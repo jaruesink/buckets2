11d6bd45bc00ba9ab6f674680df5891279c69259
Jake Ruesink <jaruesink@gmail.com>
2017-08-07 18:10:59 -0500

    two tests now working in typescript

--- backend/test/test.ts
+++ backend/test/test.ts
@@ -1,5 +1,9 @@
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
+import * as config from 'config';
+import * as logger from '../server/logger';
+
+
+import * as db from 'mongoose';
+(<any>db).Promise = global.Promise;
 
 import * as feathers from 'feathers/client';
 import * as rest from 'feathers-rest/client';
@@ -7,32 +11,73 @@ import * as fetch from 'node-fetch';
 
 import { suite, test } from 'mocha-typescript';
 import { assert } from 'chai';
-import { UserData, UserModel, UserSchema } from '../server/models/user';
+import { UserData, UserModel, UserSchema, UserType } from '../server/models/user';
 
 import TestHelper from './test_helper';
 
 const app:any = feathers().configure(rest('http://localhost:3000').fetch(fetch));
 const user_service = app.service('api/user');
 
+const db_connection_options = config.get('buckets.db.options');
+const db_connection_url = config.get('buckets.db.url');
+
+db.connect(db_connection_url, db_connection_options, (error) => {
+  if (error) { logger.debug('error with connection', error); }
+}).then(() => {
+  logger.debug('connected to:', db_connection_url);
+});
+
 
 @suite('Users') class UserTest extends TestHelper {
-  @test 'can be created'(done) {
 
-    // NO HACIENDO
-    console.log('User is a model', UserModel);
-    UserModel.count({}).then((count) => {
-      user_service.create({
-        fbid: 1234567890,
-        name: 'Test User',
-        email: 'test@email.com'
-      }, (err) => { if (err) { console.error(err); } })
-      .then((new_user) => {
-        UserModel.count({}).then((new_count) => {
-          console.log(count, new_count);
-          assert(count + 1 === new_count);
-          done();
-        });
-      });
-    });
+  userData: UserData = {
+    fbid: 1234567890,
+    name: 'Test User',
+    email: 'test@email.com'
   }
+
+  createNewUser(data): UserType {
+    return new UserModel(data);
+  }
+
+  createUserService(data) {
+    return user_service.create(data, (err) => { if (err) { console.error(err); } });
+  }
+
+  saveUser(user): Promise<UserType> {
+    return user.save();
+  }
+
+  updateUserService(id, data) {
+    return user_service.update(id, data, (err) => { if (err) { console.error(err); } });
+  }
+
+  userCount() {
+    return UserModel.count({});
+  }
+
+  @test async 'can be created'() {
+    await this.userCount().then(count => 
+      this.createUserService(this.userData).then((new_user) =>
+        this.userCount().then((new_count) => {
+          logger.debug(`(count + 1 === new_count) ${count} + 1 === ${new_count}`);
+          assert(count + 1 === new_count);
+        })
+      )
+    )
+  }
+
+  @test async 'can be edited'() {
+    const new_user = this.createNewUser(this.userData);
+    const new_name = 'Joe Tester';
+    await this.saveUser(new_user).then(() =>
+      this.updateUserService(new_user._id, {name: new_name}).then(edited_user =>
+        UserModel.findOne({_id: new_user._id}).then(found_user => {
+          logger.debug(`(found_user.name === new_name) ${found_user.name} === ${new_name}`);
+          assert(found_user.name === new_name);
+        })
+      )
+    )
+  }
+
 }
--- backend/test/test_helper.ts
+++ backend/test/test_helper.ts
@@ -1,34 +1,32 @@
-import * as mongoose from 'mongoose';
+import * as db from 'mongoose';
 
 import * as server from '../server';
 import * as logger from '../server/logger';
 
 import { suite } from 'mocha-typescript';
 
-const { buckets, users } = mongoose.connection.collections;
+const { users } = db.connection.collections;
 
-
-function dropBuckets(done:any) {
-  const collections = mongoose.connection.collections;
+async function dropCollections() {
+  const collections = db.connection.collections;
   const collections_to_drop = [];
-  for (const collection in mongoose.connection.collections) {
+  for (const collection in collections) {
     collections_to_drop.push(collections[collection].drop());
   }
-  return Promise.all([...collections_to_drop]).then(() => done())
-  .catch(error => {
-    console.error(error);
-    done()
-  });
+  await Promise.all([users.drop()]).catch((error) => {
+      if (error && error.message !== 'ns not found') {
+        logger.debug('error dropping collections', error);
+      } else {
+        return false;
+      }
+    });
 }
 
-@suite
-export default class Suite {
-  before(done:any) {
-    // logger.debug('reset test database before each test');
-    dropBuckets(done);
+@suite export default class Suite {
+  before() {
+    dropCollections();
   }
-  static after(done:any) {
-    // logger.debug('reset test database after all tests');
-    dropBuckets(done);
+  static after() {
+    dropCollections();
   }
 }
